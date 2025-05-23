7d1b82022f3dfa4d195a8d56c6cf43e0a72acc0e
woheller69 <woheller69@t-online.de>
2025-05-23 19:42:04 +0200

    code improvement

--- app/src/main/java/org/woheller69/weather/activities/ForecastCityActivity.java
+++ app/src/main/java/org/woheller69/weather/activities/ForecastCityActivity.java
@@ -168,7 +168,7 @@ public class ForecastCityActivity extends NavigationActivity implements IUpdatea
         SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         updateLocationButton = menu.findItem(R.id.menu_update_location);
         PFASQLiteHelper db = PFASQLiteHelper.getInstance(this);
-        if(prefManager.getBoolean("pref_GPS", true)==TRUE && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+        if(prefManager.getBoolean("pref_GPS", false)==TRUE && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
             updateLocationButton.setVisible(true);
             updateLocationButton.setActionView(R.layout.menu_update_location_view);
             updateLocationButton.getActionView().clearAnimation();
@@ -242,7 +242,7 @@ public class ForecastCityActivity extends NavigationActivity implements IUpdatea
                     tabLayoutMediator.attach();
                 }
                 SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                if (prefManager.getBoolean("pref_GPS", true) == TRUE && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (prefManager.getBoolean("pref_GPS", false) == TRUE && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     if (locationListenerGPS == null) {
                         Log.d("GPS", "Listener null");
                         locationListenerGPS = getNewLocationListener();
